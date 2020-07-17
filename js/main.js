e8cd5d3a08767020dc90d014241572f27b3caa53
Nathan <nathan@komcreative.com>
2020-07-17 15:13:35 -0500

    ...

---
+++ js/main.js
@@ -0,0 +1,18 @@
+console.log("%c✨ NATHAN U WAS HERE ✨", "font-size: 1.5em; font-weight: 800; background: linear-gradient(to right,  #91ebff 0%,#a1ffe9 11%,#b3ff99 23%,#e4ffa1 36%,#ffd3a1 49%,#ffbda1 62%,#ff9fc7 75%,#b5a1ff 88%,#79e0ff 100%); color: rgb(64,64,64); border-radius: 1em; padding: .5em .5em .5em .5em;");
+const RELLAX = new Rellax('.rellax');
+const RELLAXONE = new Rellax('.rellax1', {
+    wrapper:".transition1",
+    relativeToWrapper: true
+});
+const RELLAXTWO = new Rellax('.rellax2', {
+    wrapper:".transition2",
+    relativeToWrapper: true
+});
+const RELLAXTHREE = new Rellax('.rellax3', {
+    wrapper:".transition3",
+    relativeToWrapper: true
+});
+const RELLAXFOUR = new Rellax('.rellax4', {
+    wrapper:".transition4",
+    relativeToWrapper: true
+});
