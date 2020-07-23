989329660d0c9c1b67de37a2a04182fbc890fdff
Nathan <nathan@komcreative.com>
2020-07-23 15:20:33 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 console.log("%c✨ NATHAN U WAS HERE ✨", "font-size: 1.5em; font-weight: 800; background: linear-gradient(to right,  #91ebff 0%,#a1ffe9 11%,#b3ff99 23%,#e4ffa1 36%,#ffd3a1 49%,#ffbda1 62%,#ff9fc7 75%,#b5a1ff 88%,#79e0ff 100%); color: rgb(64,64,64); border-radius: 1em; padding: .5em .5em .5em .5em;");
+// Animate On Scroll
 AOS.init();
+
+// Rellax Parallax
 const RELLAX = new Rellax('.rellax');
 const RELLAXONE = new Rellax('.rellax1', {
     wrapper:".transition1",
@@ -17,3 +20,5 @@ const RELLAXFOUR = new Rellax('.rellax4', {
     wrapper:".transition4",
     relativeToWrapper: true
 });
+
+// Equity Principles Logos
