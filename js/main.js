fedabf6fa80ab94a2d0a8ae959cee0332a10a743
Nathan <nathan@komcreative.com>
2020-08-12 18:20:50 -0500

    mobile fixes

--- js/main.js
+++ js/main.js
@@ -9,47 +9,57 @@ console.log(`
     * Built with SCSS
 `)
 
-//Checks for mobile browsers
+// Legacy mobile browser check
 window.mobileCheck = () => {
     let check = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
   };
 
+// Modern mobile browser check
+const uaDataIsMobile = window.navigator.userAgentData?.mobile
+
+// Mobile browser check with legacy fallback
+const isMobile = () => {
+    if (typeof uaDataIsMobile === 'boolean') {
+        return uaDataIsMobile
+    } else {
+        return window.mobileCheck()
+    }
+}
 
 // Loads particle effect
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
-  });
+});
 
-
-
-// If browser user agent != mobile, initiate animate on scroll
-if (Window.matchMedia("(max-width: 900px)").matches) {
+// If browser user agent != mobile, initiate animations
+if (isMobile()) {
     console.log("Animate on scroll disabled");
-    getElementByTagName("nav").classList.add("hide");
-    alert("hello");
+    document.getElementsByTagName("nav")[0].classList.add("hide");
 } else {
+    //Initiates animate on scroll
     new WOW().init(); 
     console.log("Animate on Scroll enabled");
-}
+    // Allocates parent of each rellax element as a wrapper  & creates
+    // new rellax object for each to avoid huge position offsets 
+    // due to rellax element scroll speed settings ⊙.☉
+    let rellaxArr = document.getElementsByClassName("parallax");
+    let rellaxElems = [];
 
-// Rellax Parallax
-let rellaxArr = document.getElementsByClassName("parallax");
-let rellaxElems = [];
-
-for (let i = 0; i < rellaxArr.length; i++) {
-    let parallaxItem = "rellax"+i;
-    let wrapperClass = "wrapper"+i;
-    // Add classes to parallax elements & wrappers
-    rellaxArr[i].parentElement.classList.add(wrapperClass);
-    rellaxArr[i].classList.add(parallaxItem);
-    rellaxElems.push([parallaxItem,wrapperClass]);
-    // Create new rellax objects from parallax elements
-    window[rellaxElems[i][0]] = new Rellax(eval("\'." + rellaxElems[i][0] +"\'"), {
-        wrapper: eval("\'."+rellaxElems[i][1]+"\'"),
-        relativeToWrapper: true
-    });
+    for (let i = 0; i < rellaxArr.length; i++) {
+        let parallaxItem = "rellax"+i;
+        let wrapperClass = "wrapper"+i;
+        // Add classes to parallax elements & wrappers
+        rellaxArr[i].parentElement.classList.add(wrapperClass);
+        rellaxArr[i].classList.add(parallaxItem);
+        rellaxElems.push([parallaxItem,wrapperClass]);
+        // Create new rellax objects from parallax elements
+        window[rellaxElems[i][0]] = new Rellax(eval("\'." + rellaxElems[i][0] +"\'"), {
+            wrapper: eval("\'."+rellaxElems[i][1]+"\'"),
+            relativeToWrapper: true
+        });
+    }
 }
 
 // Equity Principles Logos
@@ -151,7 +161,7 @@ const LOGOS = [
     ['Zeno Group','https://dl.airtable.com/.attachments/5871d6416859891149b1503ac9b6585d/1ca6cb1b/0039.png'],
     ['Ziegler','https://dl.airtable.com/.attachments/26cb0a928265445211daa083da0f74e9/7aefa3a8/ziegler-logo.png']]
 
-// Generate <div> for each logo / company name
+// Generates a <div> for each logo / company name in a 2d array: [['name', 'logoURL']]
 const SPITLOGOS = (arr) => {
     arr.forEach(x => {
         x[1] ? document.getElementById('logos').innerHTML += ("<div class=\"EPlogo wow fadeInBottom\" data-wow-duration=\".35s\"><img src=\""+x[1]+"\" alt=\""+x[0]+" Logo\"></div>") 
@@ -159,8 +169,7 @@ const SPITLOGOS = (arr) => {
     })
 }
 
-SPITLOGOS(LOGOS);
-
+// Color class arrays: one for text, one for backgrounds, and one omitting dark pink
 let textColors = ["light-blue", "dark-blue", "yellow", "dark-dark-blue", "gray", "pink", "dark-pink"];
 let bgColors = ["bg-light-blue", "bg-dark-blue", "bg-yellow", "bg-dark-dark-blue", "bg-gray", "bg-pink", "bg-dark-pink"];
 let eventColors = ["bg-light-blue", "bg-dark-blue", "bg-yellow", "bg-dark-dark-blue", "bg-gray", "bg-pink"];
@@ -177,11 +186,6 @@ const APPLYCLASS = (targetClassName, classArr) => {
     }
 }
 
-APPLYCLASS("stat", textColors);
-APPLYCLASS("statHex", bgColors);
-APPLYCLASS("eventHex", eventColors)
-
-
 //Removes className from all elements matching className
 const SEEKANDREMOVE = (className) => {
 elems = document.getElementsByClassName(className);
@@ -189,3 +193,8 @@ elems = document.getElementsByClassName(className);
         elems[i].classList.remove(className);
     }
 }
+
+SPITLOGOS(LOGOS);
+APPLYCLASS("stat", textColors);
+APPLYCLASS("statHex", bgColors);
+APPLYCLASS("eventHex", eventColors)
